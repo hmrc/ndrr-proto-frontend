9dda734d8a4baf546f31a37790c09bb4fdb273a9
hmrc-web-operations <hmrc-web-operations@digital.hmrc.gov.uk>
2024-11-05 15:09:31 +0000

    'Creating new service ndrr-proto-frontend'

---
+++ app/uk/gov/hmrc/ndrrprotofrontend/config/Module.scala
@@ -0,0 +1,11 @@
+package uk.gov.hmrc.ndrrprotofrontend.config
+
+import com.google.inject.AbstractModule
+
+class Module extends AbstractModule {
+
+  override def configure(): Unit = {
+
+    bind(classOf[AppConfig]).asEagerSingleton()
+  }
+}
