b68cc4cb3c9a7b4087ec8168033c1a10e73950f7
adwelly <2620549+adwelly@users.noreply.github.com>
2024-11-13 15:26:33 +0000

    WIP

--- app/uk/gov/hmrc/ndrrprotofrontend/models/Address.scala
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmrc.ndrrprotofrontend.models
-
-import play.api.libs.json.{Json, OFormat}
-
-final case class Address(line1: String, line2: Option[String], town: String, county: Option[String], postcode: Postcode) extends ColumnEntry
-
-object Address {
-  implicit val format: OFormat[Address] = Json.format[Address]
-}
