0e0b781fc1c59d7d73b15ae9569d85ab600ea26e
William Hatzar <22661741+william-hatzar@users.noreply.github.com>
2024-12-17 09:49:43 +0000

    Added in radio buttons using the data structure method generically (#9)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,11 +6,13 @@ import sbt._
 object AppDependencies {
 
   private val bootstrapVersion = "9.5.0"
-  
+  private val enumeratumVersion = "1.8.1"
+
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30" % "11.3.0"
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30" % "11.3.0",
+    "com.beachape"            %% "enumeratum-play"            %  enumeratumVersion,
   )
 
   val test = Seq(
